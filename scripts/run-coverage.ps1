fe2c06e73adc29618eef1d4a0f032eeb0ac9544c
Despairoz <mijail_perez@hotmail.com>
2026-07-01 20:37:30 -0500

    Run coverage scripts directly in workflow

--- scripts/run-coverage.ps1
+++ scripts/run-coverage.ps1
@@ -5,4 +5,4 @@ param(
 $cp = ((Get-ChildItem (Join-Path $ProjectRoot 'lib') -Filter *.jar | ForEach-Object { $_.FullName }) -join ';')
 $cp = $cp + ';' + (Join-Path $ProjectRoot 'build\classes') + ';' + (Join-Path $ProjectRoot 'build\test\classes')
 
-& 'C:\Program Files\Java\jdk-25\bin\java.exe' -cp $cp tdd.CoberturaTDD
+java -cp $cp tdd.CoberturaTDD
